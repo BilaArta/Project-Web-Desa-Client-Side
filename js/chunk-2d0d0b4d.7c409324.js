626220a78457751e3c873d093e1fcb629d7e9d80
Bila Arta <bilaarta@gmail.com>
2021-03-09 17:53:08 +0700

    deploy

---
+++ js/chunk-2d0d0b4d.7c409324.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d0b4d"],{"68b7":function(n,e,l){"use strict";l.r(e);var t=function(){var n=this,e=n.$createElement,l=n._self._c||e;return l("h1",[n._v("hello")])},u=[],c={},o=c,r=l("2877"),s=Object(r["a"])(o,t,u,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0d0b4d.7c409324.js.map
